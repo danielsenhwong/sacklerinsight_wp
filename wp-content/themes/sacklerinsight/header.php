237d9b3aa3b3824c5da4521aaaebcef0fef322cc
Daniel Senh Wong <danielsenhwong@gmail.com>
2017-02-02 10:26:59 -0500

    Added Google Analytics

--- wp-content/themes/sacklerinsight/header.php
+++ wp-content/themes/sacklerinsight/header.php
@@ -8,6 +8,16 @@
     <meta name="description" content="">
     <meta name="author" content="Sackler InSight Team, Tufts Sackler School">
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-29629616-4', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 
     <?php wp_head(); # import everything in functions.php, e.g. Title, CSS, Google Fonts ?>
 </head>

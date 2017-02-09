54a273be4a03669469fbc77765fe50677b2df6fb
Daniel Wong <dsw2107@gmail.com>
2017-02-09 14:43:13 -0500

    Update content-recent.php

--- wp-content/themes/sacklerinsight/template-parts/content-recent.php
+++ wp-content/themes/sacklerinsight/template-parts/content-recent.php
@@ -6,6 +6,5 @@
     
     <h2 class="blog-post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
     <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-
-    <?php the_excerpt();?>
+    
 </div><!-- /.recent-content -->

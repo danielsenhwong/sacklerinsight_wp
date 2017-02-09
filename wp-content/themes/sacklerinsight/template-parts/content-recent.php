edbd16439f0e3af68cced9e840238653d57fa894
Daniel Wong <dsw2107@gmail.com>
2017-02-09 14:42:25 -0500

    Update content-recent.php

--- wp-content/themes/sacklerinsight/template-parts/content-recent.php
+++ wp-content/themes/sacklerinsight/template-parts/content-recent.php
@@ -1,16 +1,11 @@
 <div class="recent-content">
+    <?php
+    if ( has_post_thumbnail() ) {
+        the_post_thumbnail('medium');
+    } ?>
+    
     <h2 class="blog-post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
     <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-    <?php if ( has_post_thumbnail() ) { ?>
-        <?php the_post_thumbnail('thumbnail');?>
-        <?php the_excerpt();?>
-    <?php } else {
-        the_excerpt();
-    } ?>
-
-    <a href="<?php comments_link(); ?>">
-        <?php
-        printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
-    </a>
+    <?php the_excerpt();?>
 </div><!-- /.recent-content -->

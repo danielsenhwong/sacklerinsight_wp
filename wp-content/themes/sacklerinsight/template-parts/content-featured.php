adaba359a90aee8c2cf49901ca66006750583c64
Daniel Wong <dsw2107@gmail.com>
2017-02-09 12:29:59 -0500

    Create content-featured.php

---
+++ wp-content/themes/sacklerinsight/template-parts/content-featured.php
@@ -0,0 +1,8 @@
+<?php
+    $featured_posts = insight_get_featured_posts();
+		foreach ( (array) $featured_posts as $order => $post ) :
+			setup_postdata( $post );
+			// Include the featured content template.
+			get_template_part( 'content', 'featured-post' );
+		endforeach;
+?>
